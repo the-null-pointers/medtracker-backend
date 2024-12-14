ac28cab14f657ea32401ef345ff9220920193e0a
adkshishir <adhikarishishir50@gmail.com>
2024-12-14 19:45:38 +0545

    feat: missing imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,29 +4,26 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
-// import { MiddlewareMiddleware } from './middleware/middleware.middleware';
-// import { ProfileModule } from './profile/profile.module';
-// import { HospitalModule } from './hospital/hospital.module';
-// import { DoctorModule } from './doctor/doctor.module';
-// import { TreatmentModule } from './treatment/treatment.module';
-// import { HealthHistoryModule } from './health-history/health-history.module';
-// import { RolesModule } from './roles/roles.module';
-// import { LabReportModule } from './lab-report/lab-report.module';
-// import { MailerModule } from './mailer/mailer.module';
-// import { TwilioService } from './twilio/twilio.service';
+import { MiddlewareMiddleware } from './middleware/middleware.middleware';
+import { ProfileModule } from './profile/profile.module';
+import { HospitalModule } from './hospital/hospital.module';
+import { DoctorModule } from './doctor/doctor.module';
+import { TreatmentModule } from './treatment/treatment.module';
+import { HealthHistoryModule } from './health-history/health-history.module';
+import { RolesModule } from './roles/roles.module';
+import { LabReportModule } from './lab-report/lab-report.module';
+import { MailerModule } from './mailer/mailer.module';
+import { TwilioService } from './twilio/twilio.service';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { NotificationModule } from './notification/notification.module';
 
 @Module({
-  imports: [AuthModule,
-    PrismaModule,
-    // ProfileModule, HospitalModule, DoctorModule, TreatmentModule, HealthHistoryModule, RolesModule, LabReportModule, MailerModule
-  ],
+  imports: [AuthModule, PrismaModule, ProfileModule, HospitalModule, DoctorModule, TreatmentModule, HealthHistoryModule, RolesModule, LabReportModule, MailerModule, DashboardModule, NotificationModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService,
-    // TwilioService
-  ],
+  providers: [AppService, PrismaService, TwilioService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    // consumer.apply(MiddlewareMiddleware).forRoutes('*'); // Apply to all routes in this module
+    consumer.apply(MiddlewareMiddleware).forRoutes('*'); // Apply to all routes in this module
   }
 }
